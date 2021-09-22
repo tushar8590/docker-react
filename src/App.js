fced1f05f317ed9b501b065f676ad5ae5313af80
Tushar Srivastava <tushar.srivastava@tadigital.com>
2021-09-22 23:17:55 +0530

    App text change

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         The changes made in feature branch
         </a>
       </header>
     </div>
